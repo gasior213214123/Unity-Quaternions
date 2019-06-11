e65491c88c46b798e4cc46c64230e2fc90bbb19f
gasior213214123 <szczepan.lis@gmial.com>
2019-06-11 18:24:48 +0200

    Source files

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class LookAt : MonoBehaviour
+{  
+    public Transform target;
+    public Transform obiekt;
+    public Text eulerText;
+    public Text quaternionsText;
+    ///private Text quaternions;
+
+
+    void Update()
+    {
+        Quaternion current = transform.localRotation;
+        quaternionsText.text = "Kwaternion szescianu: " + current;
+        eulerText.text = "Euler szescianu: (" + current.eulerAngles.x + ", " + current.eulerAngles.y + ", " + current.eulerAngles.z + ")";
+
+        Vector3 relativePos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text eulerText;
+    public Text quaternionsText;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion current = transform.localRotation;
+        quaternionsText.text = "Kwaternion sfery: " + current;
+        eulerText.text = "Euler sfery: (" + current.eulerAngles.x + ", " + current.eulerAngles.y + ", " + current.eulerAngles.z + ")";
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
